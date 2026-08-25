2da9378e291395467eb2c249967e3da6f0680982
John Kommala <59068112+jonwk@users.noreply.github.com>
2026-08-25 21:06:55 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10         # int type
+print(type(variable))
+
+variable = 1.10         # int type
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "10"         # int type
+print(type(variable))
+
+variable = [10]        # int type
+print(type(variable))
