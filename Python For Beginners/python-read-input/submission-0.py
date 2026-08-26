91babd078dd7fa8e60729805ebbf91ff772c8962
John Kommala <59068112+jonwk@users.noreply.github.com>
2026-08-26 22:56:02 +0100

    Add: python-read-input - submission-0

---
+++ Python For Beginners/python-read-input/submission-0.py
@@ -0,0 +1,8 @@
+def read_and_print_input() -> None:
+    input_str = "Reading one line of text into a string: "
+    user_input = input(input_str)
+    print(user_input)
+
+
+read_and_print_input()
+read_and_print_input()
